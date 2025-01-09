d1eb15293e03ccabc1bc1f0604864b2ff4200da5
Ami <amichecks@gmail.com>
2025-01-09 14:08:08 +0200

    slippage-test

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ setup(
     install_requires=[
         "web3>=6.0.0",
     ],
+    extras_require={
+        "dev": [
+            "pytest>=7.1.1",
+            "pytest-cov>=3.0.0",
+            "pytest-asyncio>=0.21.1",
+        ],
+    },
     python_requires=">=3.8",
 
     author="ami@ostium.io",
