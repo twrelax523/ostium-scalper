1fd4eb998d054ac936e03027ebf1f0653bd8f42d
Ami <amichecks@gmail.com>
2025-03-04 22:30:00 +0200

    ver 2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if changelog_path.exists():
 
 setup(
     name="ostium-python-sdk",
-    version="0.2.107",
+    version="2.0.0",
     packages=find_packages(),
     install_requires=read_requirements('requirements.txt'),
     extras_require={
