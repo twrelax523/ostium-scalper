b5b5fef1829feb8413c4b44577be7f3e39ae1fc4
Ami <amichecks@gmail.com>
2025-05-26 21:31:41 +0300

    add case #2 test for tests/test_get_trade_value.py

--- tests/test_get_trade_value.py
+++ tests/test_get_trade_value.py
@@ -17,12 +17,22 @@ test_cases = [
         'funding_fee': Decimal('0'),  # 0 / PRECISION_6
         'leverage': Decimal('10'),  # 1000 / PRECISION_2
         'max_leverage': Decimal('10'),  # 1000 / PRECISION_2
-        # 100999999 / PRECISION_6
-        'expected_trade_value': Decimal('100.999999'),
+        'expected_trade_value': Decimal('100.999999'), # 100999999 / PRECISION_6
+        'expected_liq_margin_value': Decimal('25'),  # 25000000 / PRECISION_6
+    },
+    # CASE-1
+    {
+        'liq_margin_threshold_p': Decimal('25'),  # 25 (no division needed)
+        'collateral': Decimal('100'),  # 100000000 / PRECISION_6
+        'percent_profit': Decimal('87.769211'),  # 87769211 / PRECISION_6
+        'rollover_fee': Decimal('1.395969'),  # 1395969 / PRECISION_6
+        'funding_fee': Decimal('2.145623'),  # 2145623 / PRECISION_6
+        'leverage': Decimal('10'),  # 1000 / PRECISION_2
+        'max_leverage': Decimal('10'),  # 1000 / PRECISION_2
+        'expected_trade_value': Decimal('184.227619'),  # 184227619 / PRECISION_6
         'expected_liq_margin_value': Decimal('25'),  # 25000000 / PRECISION_6
     }
 ]
-
 
 @pytest.mark.parametrize("case", test_cases)
 def test_get_trade_value(case):
@@ -41,7 +51,7 @@ def test_get_trade_value(case):
     # Assert trade value
     assert trade_value == pytest.approx(case['expected_trade_value'], abs=1e-5), \
         f"Trade value failed for case: {case}"
-
+    
     # Assert liquidation margin value
     assert liq_margin_value == pytest.approx(case['expected_liq_margin_value'], abs=1e-5), \
         f"Liquidation margin value failed for case: {case}"
